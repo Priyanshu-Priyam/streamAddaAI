fa0ccd9c59b8425b4eecd196bb9d03b7e68047b0
Priyanshu Priyam <79155933+Priyanshu-Priyam@users.noreply.github.com>
2024-07-01 16:54:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import openai
+import base64
+
+
+
+MODEL ="gpt-4o"
 
 def summarize_conversation(api_key, text):
     client = openai.OpenAI(api_key=api_key)
@@ -13,7 +18,7 @@ def summarize_conversation(api_key, text):
     
     try:
         response = client.chat.completions.create(
-            model="gpt-3.5-turbo-0125",  # Ensure consistency in model usage
+            model="gpt-3.5-turbo-0125",
             messages=[
                 {"role": "system", "content": prompt},
                 {"role": "assistant", "content": " "}
@@ -23,11 +28,50 @@ def summarize_conversation(api_key, text):
     except Exception as e:
         return f"An error occurred: {str(e)}"
 
-def get_response(api_key, message, context):
+def get_response(api_key, message, context, image_b64=None):
     client = openai.OpenAI(api_key=api_key)
-    full_prompt = f"This is the summary of previous responses:\n{context}\n\nThis is the current question and answer pair:\nUser: {message}\nAssistant:"
-    
-    try:
+    if message and image_b64:
+        
+        base64_image = image_b64
+        
+        response = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": f"You are a helpful assistant that responds in Markdown, help the user with his/her query.  Here is the summary of previous conversation with this user : {context}"},
+                {"role": "user", "content": [
+                    {"type": "text", "text": message},
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:image/png;base64,{base64_image}"}
+                    }
+                ]}
+            ],
+            temperature=0.0,
+        )
+        
+        return response.choices[0].message.content
+        
+    elif image_b64:
+                
+        base64_image = image_b64
+        
+        response = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": f"You are a helpful assistant that responds in Markdown, help the user with his/her query. Here is the summary of previous conversation with this user : {context} "},
+                {"role": "user", "content": [
+                    {"type": "text", "text": "Solve it"},
+                    {"type": "image_url", "image_url": {
+                        "url": f"data:image/png;base64,{base64_image}"}
+                    }
+                ]}
+            ],
+            temperature=0.0,
+        )
+        
+        return response.choices[0].message.content
+        
+    else:
+        full_prompt = f"This is the summary of previous responses:\n{context}\n\nThis is the current question and answer pair:\nUser: {message}\nAssistant:"    
         response = client.chat.completions.create(
             model="gpt-3.5-turbo-0125",
             messages=[
@@ -37,8 +81,11 @@ def get_response(api_key, message, context):
             ],
         )
         return response.choices[0].message.content
-    except Exception as e:
-        return f"An error occurred: {str(e)}"
+
+
+def encode_image_to_base64(image):
+    
+    return base64.b64encode(image.getvalue()).decode()
 
 def main():
     st.title('AddaAI Chatbot')
@@ -59,14 +106,19 @@ def main():
             st.text_area("Answer:", value=answer, height=150, key=answer + "_a")
 
         user_input = st.text_input("Type your message here:", key="user_input")
+        uploaded_file = st.file_uploader("Upload an image related to your doubt", type=['png', 'jpg', 'jpeg'], key="file_uploader")
 
         if st.button('Send'):
-            if user_input and api_key:
-                response = get_response(api_key, user_input, st.session_state['summary'])
+            image_b64 = ""
+            if uploaded_file is not None:
+                image = Image.open(uploaded_file)
+                image_b64 = encode_image_to_base64(image)
+
+            if api_key and (user_input or image_b64):
+                response = get_response(api_key, user_input, st.session_state['summary'], image_b64)
                 st.session_state['chat_history'].append((user_input, response))
                 
                 if len(st.session_state['chat_history']) % 5 == 0:
-                    # Summarize the latest conversations or all conversations if this is the first summary
                     chat_text = "\n".join([f"User: {q}\nAssistant: {a}" for q, a in st.session_state['chat_history']])
                     new_summary = summarize_conversation(api_key, chat_text)
                     st.session_state['summary'] = summarize_conversation(api_key, f"{st.session_state['summary']}\n{new_summary}")
@@ -74,7 +126,7 @@ def main():
 
                 st.experimental_rerun()  # Rerun the app to clear the input and refresh the chat history
             else:
-                st.error("Both API key and a message are required to send.")
+                st.error("An API key and at least a message or image are required to send.")
 
 if __name__ == '__main__':
     main()
