18cc10f6cc96bcd19192ebc83b4b17f2b25036df
Priyanshu Priyam <79155933+Priyanshu-Priyam@users.noreply.github.com>
2024-07-01 18:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,5 +137,6 @@ def main():
             else:
                 st.error("An API key and at least a message or image are required to send.")
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
+
